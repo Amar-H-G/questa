892e09d9188b5f59dcc13cde9512d355ccd794ee
Amar Patra <amarpatra932@gmail.com>
2026-06-13 14:52:49 +0530

    try to fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,6 +38,18 @@ app.use(
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 app.use(cookieParser());
+// Workaround for Express 5 + express-mongo-sanitize compatibility: make req.query writable
+app.use((req, res, next) => {
+  if (req.query) {
+    Object.defineProperty(req, 'query', {
+      value: { ...req.query },
+      writable: true,
+      configurable: true,
+      enumerable: true,
+    });
+  }
+  next();
+});
 app.use(mongoSanitize());
 app.use(hpp());
 app.use(compression());

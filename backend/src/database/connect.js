adf1b31a0b05c5f1eca54ddf3e94c464720da947
Amar Patra <amarpatra932@gmail.com>
2026-06-14 09:19:40 +0530

    trying to fix

--- backend/src/database/connect.js
+++ backend/src/database/connect.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
 const env = require('../config/env');
+
+// Fix DNS resolution issues for MongoDB SRV records on Windows/local networks
+if (env.MONGO_URI && env.MONGO_URI.startsWith('mongodb+srv://')) {
+  try {
+    dns.setServers(['8.8.8.8', '1.1.1.1']);
+  } catch (err) {
+    console.warn('Warning: Could not set custom DNS servers for SRV resolution:', err.message);
+  }
+}
 
 const connectDatabase = async () => {
   mongoose.set('strictQuery', true);
